c56f14d7d6df7673ed6bce804c7aebd62fd7da84
E.Iliadis <vagelisiliadis93@gmail.com>
2022-12-14 13:29:08 +0200

    Discovery Server App added, dependencies added

---
+++ discovery-server/src/main/java/com/music/microservices/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.music.microservices.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
