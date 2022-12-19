25d9d5e54356f32d5e4c694f4404dadc00d46f42
e.iliadis <e.iliadis@pmmretail.com>
2022-12-19 14:22:13 +0200

    Add circuit breaker

--- soundtrack-service/src/main/java/com/com/music/microservices/soundtrackservice/controller/SoundtrackController.java
+++ soundtrack-service/src/main/java/com/com/music/microservices/soundtrackservice/controller/SoundtrackController.java
@@ -3,6 +3,7 @@ package com.com.music.microservices.soundtrackservice.controller;
 import com.com.music.microservices.soundtrackservice.dto.SoundtrackDto;
 import com.com.music.microservices.soundtrackservice.model.Soundtrack;
 import com.com.music.microservices.soundtrackservice.service.SoundtrackService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ public class SoundtrackController {
 
 
     @PostMapping(consumes = {"multipart/form-data"})
+    @CircuitBreaker(name = "upload", fallbackMethod = "fallbackMethod")
     public ResponseEntity<?> upload(@RequestPart("soundtrackDto") SoundtrackDto soundtrackDto, @RequestPart("file") MultipartFile multipartFile) throws IOException {
         String uploadSong = soundtrackService.uploadSoundtrack(multipartFile, soundtrackDto);
         return ResponseEntity.status(HttpStatus.OK)
@@ -27,6 +29,7 @@ public class SoundtrackController {
     }
 
     @GetMapping(value = "/download/{fileName}" , produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @CircuitBreaker(name = "download", fallbackMethod = "fallbackMethod")
     public HttpEntity<byte[]> download(@PathVariable String fileName) throws IOException {
         byte[] content = soundtrackService.downloadSoundtrack(fileName);
         HttpHeaders header = new HttpHeaders();
@@ -45,5 +48,9 @@ public class SoundtrackController {
     @GetMapping("findByName")
     public ResponseEntity<Soundtrack> getSongByName(@RequestParam String name) {
         return ResponseEntity.ok().body(soundtrackService.findSongByName(name));
+    }
+
+    public String fallbackMethod(SoundtrackDto soundtrackDto, RuntimeException runtimeException){
+        return "Something went wrong please try again after some time";
     }
 }
