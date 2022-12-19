dbec73eca3afe4cf76f10d96bac2f5f3c379d6c5
e.iliadis <e.iliadis@pmmretail.com>
2022-12-19 13:23:47 +0200

    Add updated security configuration

--- discovery-server/src/main/java/com/music/microservices/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/music/microservices/discoveryserver/config/SecurityConfig.java
@@ -3,21 +3,21 @@ package com.music.microservices.discoveryserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Value("${eureka.username}")
     private String username;
@@ -26,13 +26,42 @@ public class SecurityConfig{
     private String password;
 
     @Bean
-    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername(username)
-                .password(bCryptPasswordEncoder.encode(password))
+                .password(passwordEncoder().encode(password))
                 .roles("USER")
                 .build());
         return manager;
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return   httpSecurity.csrf().disable()
+                .exceptionHandling()
+                .and()
+                .authorizeHttpRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
+    }
+
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider(){
+//        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService());
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return authenticationProvider;
+//    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
