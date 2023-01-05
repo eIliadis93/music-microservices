9dde8170b9e6742296f8c093ecbb9d66de487e98
E.Iliadis <vagelisiliadis93@gmail.com>
2023-01-05 19:02:50 +0200

    Refactor front end, create authorization with keycloack

---
+++ front-end/MusicShelf/src/app/sso-config.ts
@@ -0,0 +1,10 @@
+import { AuthConfig } from "angular-oauth2-oidc";
+
+export const authCodeFlowConfig: AuthConfig = {
+    issuer: '',
+    redirectUri: '',
+    clientId: '',
+    responseType: '',
+    scope: '',
+    showDebugInformation: true
+};
